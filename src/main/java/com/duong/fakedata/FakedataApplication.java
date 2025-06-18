ac146c094f3b90e0fd0cfa38f01c85518977d796
Duong Aity <duongaity@gmail.com>
2025-06-18 10:21:27 +0700

    init project

---
+++ src/main/java/com/duong/fakedata/FakedataApplication.java
@@ -0,0 +1,13 @@
+package com.duong.fakedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakedataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakedataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duong/fakedata/FakedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duong.fakedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakedataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
